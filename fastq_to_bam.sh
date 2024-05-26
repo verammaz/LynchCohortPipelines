d4617355dfd695b909d98b8b98b4ede4c656031f
verammaz <110197814+verammaz@users.noreply.github.com>
2024-05-26 19:47:25 -0400

    Changed the tools in the pipeline and added error handling

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
+#################################################################################################################
+#specify file path for sites_of_variation.vcf (required for GATK BaseRecalibrator and subsequent GATK ApplyBQSR)
+SITES_OF_VARIATION="" 
+#################################################################################################################
+
+# Function to print progress with timestamp
+print_progress() {
+    echo "[`date +%Y-%m-%dT%H:%M:%S`] $1"
+}
+
 # Load required modules
-module load samtools
-module load bwa
-module load gatk
-module load java
+module purge
+print_progress "Loading required modules..."
+module load samtools >/dev/null 2>&1
+module load bwa >/dev/null 2>&1
+module load gatk >/dev/null 2>&1
+module load java/1.8.0_66 >/dev/null 2>&1
+module load picard/2.2.4 >/dev/null 2>&1
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
 # Function to print usage
 usage() {
-    printf "Usage: $0 [-v] -f <reference.fa> -r <read1.fastq> -r <read2.fastq> -o <output_prefix>\n
-    \t [-v] flag for viewing alignment immediately \n
-    \t -f reference genome file \n 
-    \t -r read fastq files can be zipped or unzipped (two files required) \n
-    \t -o output file prefix\n"
+    cat << EOF
+Usage: $0 [options] -f <reference.fa> -r <read1.fastq,read2.fastq> -o <output_prefix>
+
+Required Arguments:
+  -f <reference.fa>                Reference genome file in FASTA format.
+  -r <read1.fastq,read2.fastq>     Comma-separated list of two FASTQ files.
+  -o <output_prefix>               Prefix for output files.
+
+Options:
+  -h                               Display this message.
+  -v                               Enable verbose mode.
+  --run_index                      Run bwa_index step (required for bwa_mem alignment, once per reference genome)
+  --keep_intermediate              Keep intermediate files generated during the pipeline.
+  --view_final                     View final BAM file using samtools and less.
+  --post_process                   Carry out post processing of BAM file after alignment and sorting (mark duplicates, base quality recallibration)
+
+
+EOF
     exit 1
 }
 
@@ -23,113 +49,140 @@ usage() {
 READS=()
 REFERENCE=
 OUTPUT_PREFIX=
-VIEW="false"
-BAM=
+VIEW=0
+VERBOSE=0
+INDEX=0
+POST_PROCESS=0
+KEEP_INTERMEDIATE=0
 
-# Parse command line arguments
-while getopts 'f:r:o:vh' option; do
-    case "$option" in
-        f)
-            REFERENCE="$OPTARG"
-            ;;
-        r)
-            READS+=("$OPTARG")
-            ;;
-        o)
-            OUTPUT_PREFIX="$OPTARG"
-            ;;
-        v)
-            VIEW=true
-            ;;
-        h | *)
-            usage
-            ;;
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -h) usage ;;
+        -v) VERBOSE=1 ;;
+        --keep_intermediate) KEEP_INTERMEDIATE=1 ;;
+        --view_final) VIEW=1 ;;
+        --post_process) POST_PROCESS=1 ;;
+        --run_index) INDEX=1 ;;
+        -f) REFERENCE="$2"; shift ;;
+        -r) READS="$2"; shift ;;
+        -o) OUTPUT_PREFIX="$2"; shift ;;
+        *) echo "Error: Unkown argument/option: $1" ; usage ;;
     esac
+    shift
 done
 
-# Ensure all required arguments are provided
-if [[ -z $REFERENCE || ${#READS[@]} -ne 2 || -z $OUTPUT_PREFIX ]]; then
-    echo "Not all required arguments provided"
+# Check that mandatory arguments are provided
+if [ -z "$REFERENCE" ] || [ -z "$READS" ] || [ -z "$OUTPUT_PREFIX" ]; then
+    echo "Error: Not all required arguemnts provided."
     usage
 fi
 
+# Split the READS argument into an array
+IFS=',' read -r -a READ_ARRAY <<< "$READS"
 
-# Check if output prefix includes .bam
-if [[ "$OUTPUT_PREFIX" == *.bam ]]; then
-   BAM=$OUTPUT_PREFIX
-else
-    BAM="${OUTPUT_PREFIX}.bam"
+# Ensure exactly two read files are provided
+if [ ${#READ_ARRAY[@]} -ne 2 ]; then
+    echo "Error: Exactly two read files must be specified, separated by a comma."
+    usage
 fi
 
-# Function to print the current progress
-print_progress() {
-    echo "[`date +%Y-%m-%dT%H:%M:%S`] $1"
-}
-
-# Function to unzip fastq file
-handle_unzipping() {
-    local read_file=$1
-    local unzipped_file=$2
-
-    if [[ "$read_file" == *.gz ]]; then
-            print_progress "Unzipping $read_file..."
-            gunzip -c "$read_file" > "$unzipped_file"
-    else
-        unzipped_file=$read_file
-        print_progress "$unzipped_file already exists. Skipping unzipping."
-    fi
-}
+READS_1=${READ_ARRAY[0]}
+READS_2=${READ_ARRAY[1]}
 
 
-# Check if the final BAM file already exists
-if [ -f "$FINAL_BAM" ]; then
-    print_progress "Final BAM file $BAM already exists."
+# If verbose mode is enabled, print the parameters
+if [ $VERBOSE -eq 1 ]; then
 
-# Run fastq-->bam pipline
-else
-    READS_1=${READS[0]}
-    READS_2=${READS[1]}
+    # Convert 1/0 to true/false for printing
+    INDEX_STR=$( [ $INDEX -eq 1 ] && echo "true" || echo "false" )
+    KEEP_INTERMEDIATE_STR=$( [ $KEEP_INTERMEDIATE -eq 1 ] && echo "true" || echo "false" )
+    POST_PROCESS_STR=$( [ $POST_PROCESS -eq 1 ] && echo "true" || echo "false" )
+    VIEW_STR=$( [ $VIEW -eq 1 ] && echo "true" || echo "false" )
 
-    print_progress "Starting the pipeline: Aligning reads, sorting, marking duplicates, calibrating,mand creating final BAM..."
+    echo "---------------------------------------------"
+    echo "Reference: $REFERENCE"
+    echo "Read1: $READS_1"
+    echo "Read2: $READS_2"
+    echo "Output Prefix: $OUTPUT_PREFIX"
+    echo "Run Indexing: $INDEX_STR"
+    echo "Post Processing: $POST_PROCESS_STR"
+    echo "Keep Intermediate Files: $KEEP_INTERMEDIATE_STR"
+    echo "View Final File: $VIEW_STR"
+    echo "---------------------------------------------"
+fi
 
-    set -o pipefail 
-    
-   # Create named pipes
-    mkfifo sorted.bam marked_duplicates.bam recal_data.table
 
-    # Step 1: BWA MEM and Samtools sort
-    bwa mem -t 8 $REFERENCE $READS_1 $READS_2 | samtools sort -l 1 -@8 -o sorted.bam -T /tmp/example_prefix -
-    wait  
+# Run fastq-->bam pipeline
+
+echo "---------------------------------------------"
+print_progress "Starting the pipeline..."
+echo "---------------------------------------------"
+
+set -o pipefail 
+
+if [ $INDEX -eq 1 ]; then
+    print_progress "Indexing the reference..."
+    bwa index -a bwtsw $REFERENCE
+fi
+
+# Step 1: BWA MEM and Samtools sort
+print_progress "Aligning and sorting..."
+bwa mem -t 8 $REFERENCE $READS_1 $READS_2 | samtools sort -l 1 -@8 -o "${OUTPUT_PREFIX}_sorted.bam" -
+wait  
+
+if [ $POST_PROCESS -eq 1 ]; then
 
     # Step 2: Picard MarkDuplicates
-    java -jar picard.jar MarkDuplicates \
-        I=sorted.bam \
-        O=marked_duplicates.bam
+    print_progress "Marking duplictaes (Picard MarkDuplicates)..."
+    java -jar $PICARD MarkDuplicates \
+        I="${OUTPUT_PREFIX}_sorted.bam" \
+        O="${OUTPUT_PREFIX}_marked_dup.bam" \
+        M="${OUTPUT_PREFIX}_marked_dup_metrics.txt"
     wait  
 
-    # Step 3: GATK BaseRecalibrator
-    gatk BaseRecalibrator \
-        -I marked_duplicates.bam \
-        -R $REFERENCE \
-        --known-sites sites_of_variation.vcf \
-        -O recal_data.table
-    wait  
+    if [ -f "$SITES_OF_VARIATION" ]; then
 
-    # Step 4: GATK ApplyBQSR
-    gatk ApplyBQSR \
-        -R $REFERENCE \
-        -I marked_duplicates.bam \
-        --bqsr-recal-file recal_data.table \
-        -O $BAM
+        # Step 3: GATK BaseRecalibrator
+        print_progress "Recalibrating bases (GATK BaseRecalibrator)..."
+        gatk BaseRecalibrator \
+            -I "${OUTPUT_PREFIX}_marked_dup.bam" \
+            -R $REFERENCE \
+            --known-sites sites_of_variation.vcf \
+            -O "${OUTPUT_PREFIX}_recal_data.table"
+        wait  
 
-    # Clean up named pipes
-    rm sorted.bam marked_duplicates.bam recal_data.table
+        # Step 4: GATK ApplyBQSR
+        print_progress "Applying base recalibaration (GATK ApplyBQSR)..."
+        gatk ApplyBQSR \
+            -R $REFERENCE \
+            -I "${OUTPUT_PREFIX}_marked_dup.bam" \
+            --bqsr-recal-file "${OUTPUT_PREFIX}_recal_data.table" \
+            -O "${OUTPUT_PREFIX}_final.bam"
+    
+    else
+        echo "Sites of variation file $SITES_OF_VARIATION not found."
+        echo "Cannot run GATK BaseRecalibrator without sites_of_variation.vcf file."
+        echo "Terminating post processing after Picard MarkDuplicates..."
+        mv "${OUTPUT_PREFIX}_marked_dup.bam" "${OUTPUT_PREFIX}_final.bam"
+    fi
+else
+    mv "${OUTPUT_PREFIX}_sorted.bam" "${OUTPUT_PREFIX}_final.bam"
+fi
 
-    print_progress "Pipeline completed successfully. Output is in $BAM."
+print_progress "Pipeline completed successfully. Output is in ${OUTPUT_PREFIX}_final.bam."
+
+
+# Remove intermediate files if necessary
+if [ $KEEP_INTERMEDIATE -eq 0 ] && [ $POST_PROCESS -eq 1 ]; then
+    print_progress "Removing intermediate files..."
+    rm -f "${OUTPUT_PREFIX}_sorted.bam", "${OUTPUT_PREFIX}_marked_dup.bam", "${OUTPUT_PREFIX}_recal_table.table", "${OUTPUT_PREFIX}_marked_dup_metrics.txt"
 fi
 
 # View the final BAM file if the -v flag is set
-if [ "$VIEW" = true ]; then
+if [ $VIEW -eq 1 ]; then
     print_progress "Viewing the final BAM file..."
-    samtools view -h $BAM | less -S
+    samtools view -h "${OUTPUT_PREFIX}_final.bam" | less -S
 fi
+
+exit 1
