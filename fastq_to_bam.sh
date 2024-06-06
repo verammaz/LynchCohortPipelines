01839b25b44eac8724845977317ed6295023a8ec
verammaz <110197814+verammaz@users.noreply.github.com>
2024-06-06 01:20:51 -0400

    decreased runtime: bwa-mem threading and addition of read group; replaced piccard MarkDuplicates and AddOrReplaceReadGroups with gatk MarkDuplicatesSpark
    Will work on making number of threads in bwa-mem a parameter that can be specified by the user; Will investigate if there are other ways to improve efficiency of alignment (gpu instead of cpu, splitting files, etc.) Looking in to ways to decrease runtime of base recalibration steps (currently, take ~1 hr)

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -2,7 +2,7 @@
 
 ##############################################
 #specify file path for sites_of_variation.vcf 
-SITES_OF_VARIATION="dbsnp_138.b37.vcf"  #(this file compatible with grch37/b37 assembly)
+SITES_OF_VARIATION="ref_b37/dbsnp_138.b37.vcf"  #(this file compatible with grch37/b37 assembly)
 ##############################################
 
 # Function to print progress with timestamp
@@ -16,8 +16,6 @@ print_progress "Loading required modules..."
 module load samtools >/dev/null 2>&1
 module load bwa >/dev/null 2>&1
 module load gatk >/dev/null 2>&1
-module load java/1.8.0_66 >/dev/null 2>&1
-module load picard/2.2.4 >/dev/null 2>&1
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -29,16 +27,16 @@ Usage: $0 [options] -f <reference.fa> -r <read1.fastq,read2.fastq> -o <output_pr
 
 Required Arguments:
   -f <reference.fa>                Reference genome file in FASTA format.
-  -r <read1.fastq,read2.fastq>     Comma-separated list of two FASTQ files (no space).
+  -r <read1.fastq,read2.fastq>     Comma-separated list of two FASTQ files.
   -o <output_prefix>               Prefix for output files.
 
 Options:
   -h                               Display this message.
   -v                               Enable verbose mode.
-  --run_index                      Run bwa_index step (required for bwa_mem alignment, once per reference genome)
-  --keep_intermediate              Keep intermediate files generated during the pipeline.
-  --view_final                     Index refernece and final BAM file for IGV viewing.
-  --post_process                   Carry out post processing of BAM file after alignment and sorting (mark duplicates, base quality recallibration)
+  --index_ref                      Run bwa_index step.
+  --keep_intermediate              Keep intermediate files.
+  --igv                            Index BAM files for IGV viewing.
+  --post_process                   Carry out post processing of BAM file after alignment.
 
 
 EOF
@@ -49,7 +47,7 @@ EOF
 READS=()
 REF_FASTA=
 OUTPUT_PREFIX=
-VIEW=0
+IGV=0
 VERBOSE=0
 INDEX=0
 POST_PROCESS=0
@@ -61,9 +59,9 @@ while [[ "$#" -gt 0 ]]; do
         -h) usage ;;
         -v) VERBOSE=1 ;;
         --keep_intermediate) KEEP_INTERMEDIATE=1 ;;
-        --view_final) VIEW=1 ;;
+        --igv) IGV=1 ;;
         --post_process) POST_PROCESS=1 ;;
-        --run_index) INDEX=1 ;;
+        --index_ref) INDEX=1 ;;
         -f) REF_FASTA="$2"; shift ;;
         -r) READS="$2"; shift ;;
         -o) OUTPUT_PREFIX="$2"; shift ;;
@@ -110,7 +108,7 @@ if [ $VERBOSE -eq 1 ]; then
     INDEX_STR=$( [ $INDEX -eq 1 ] && echo "true" || echo "false" )
     KEEP_INTERMEDIATE_STR=$( [ $KEEP_INTERMEDIATE -eq 1 ] && echo "true" || echo "false" )
     POST_PROCESS_STR=$( [ $POST_PROCESS -eq 1 ] && echo "true" || echo "false" )
-    VIEW_STR=$( [ $VIEW -eq 1 ] && echo "true" || echo "false" )
+    IGV_STR=$( [ $IGV -eq 1 ] && echo "true" || echo "false" )
 
     echo "---------------------------------------------"
     echo "Reference: $REF_FASTA"
@@ -120,7 +118,7 @@ if [ $VERBOSE -eq 1 ]; then
     echo "Run Indexing: $INDEX_STR"
     echo "Post Processing: $POST_PROCESS_STR"
     echo "Keep Intermediate Files: $KEEP_INTERMEDIATE_STR"
-    echo "View Final File: $VIEW_STR"
+    echo "Produce IGV Files: $IGV_STR"
     echo "---------------------------------------------"
 fi
 
@@ -128,10 +126,8 @@ fi
 # Run fastq-->bam pipeline
 
 # File Names
-SORTED_BAM="${OUTPUT_PREFIX}_sorted.bam"
-MARKDUP_TXT="${OUTPUT_PREFIX}_markdup_metrics.txt"
+RAW_BAM="${OUTPUT_PREFIX}_raw.bam"
 MARKDUP_BAM="${OUTPUT_PREFIX}_markdup.bam"
-MARKDUP_RG_BAM="${MARKDUP_BAM}_rg.bam"
 RECAL="${OUTPUT_PREFIX}_recal_data.table"
 FINAL_BAM="${OUTPUT_PREFIX}_final.bam"
 
@@ -146,19 +142,35 @@ if [ $INDEX -eq 1 ]; then
     bwa index -a bwtsw $REF_FASTA
 fi
 
-#Step 1: BWA MEM and Samtools sort
-print_progress "Aligning and sorting..."
-bwa mem -M -t 8 $REF_FASTA $READS_1 $READS_2 | samtools sort -@8 -o $SORTED_BAM -
-wait  
+
+# Function to read the first line of a file, handling gzipped and non-gzipped files
+read_first_line() {
+    local file=$1
+    if [[ $file == *.gz ]]; then
+        gunzip -c "$file" | head -n 1
+    else
+        head -n 1 "$file"
+    fi
+}
+
+# Get the header from the first reads fastq file
+header=$(read_first_line "$READS_1")
+# Extract and format the read group ID
+id=$(echo "$header" | cut -f 1-3 -d":" | sed 's/@//' | sed 's/:/_/g')
+
+# Step 1: BWA MEM Alignment
+bwa mem -t 48 $REF_FASTA $READS_1 $READS_2 \
+        -R "@RG\tID:${id}\tPL:ILLUMINA" | samtools view -bS - > $RAW_BAM
+wait    
 
 if [ $POST_PROCESS -eq 1 ]; then
 
-    # Step 2: Picard MarkDuplicates
-    print_progress "Marking duplictaes (Picard MarkDuplicates)..."
-    java -jar $PICARD MarkDuplicates \
-        I=$SORTED_BAM \
-        O=$MARKDUP_BAM \
-        M=$MARKDUP_TXT
+    # Step 2: GATK MarkDuplicatesSpark
+    print_progress "Marking duplictates (GATK MarkDuplicatesSpark)..."
+    gatk MarkDuplicatesSpark \
+        -I $RAW_BAM \
+        -O $MARKDUP_BAM \
+        --QUIET --spark-verbosity WARN
     wait  
 
     if [ -f "$SITES_OF_VARIATION" ]; then
@@ -170,17 +182,10 @@ if [ $POST_PROCESS -eq 1 ]; then
             gatk IndexFeatureFile -I $SITES_OF_VARIATION
         fi
 
-        # Step 3 Prep: GATK AddOrReplaceReadGroups (GATK BaseRecalibrator is read group aware)
-        print_progress "Adding read groups (Picard AddOrReplaceReadGroups)..."
-        java -jar $PICARD AddOrReplaceReadGroups \
-            I=$MARKDUP_BAM \
-            O=$MARKDUP_RG_BAM \
-            RGID=4 RGLB=lib1 RGPL=ILLUMINA RGPU=unit1 RGSM=20
-
         #Step 3: GATK BaseRecalibrator
         print_progress "Recalibrating bases (GATK BaseRecalibrator)..."
         gatk BaseRecalibrator \
-            -I $MARKDUP_RG_BAM \
+            -I $MARKDUP_BAM \
             -R $REF_FASTA \
             --known-sites $SITES_OF_VARIATION \
             -O $RECAL
@@ -189,19 +194,18 @@ if [ $POST_PROCESS -eq 1 ]; then
         # Step 4: GATK ApplyBQSR
         print_progress "Applying base recalibaration (GATK ApplyBQSR)..."
         gatk ApplyBQSR \
-            -R $REF_FASTA \
-            -I $MARKDUP_RG_BAM \
+            -I $MARKDUP_BAM \
             --bqsr-recal-file $RECAL \
             -O $FINAL_BAM
     
     else
         echo "WARNING: Sites of variation file $SITES_OF_VARIATION not found."
         echo "WARNING: Cannot run GATK BaseRecalibrator."
-        print_progress "Terminating post processing after Picard MarkDuplicates..."
+        print_progress "Terminating post processing after GATK MarkDuplicatesSpark..."
         mv $MARKDUP_BAM $FINAL_BAM
     fi
 else
-    mv $SORTED_BAM $FINAL_BAM
+    mv $RAW_BAM $FINAL_BAM
 fi
 
 print_progress "Pipeline completed successfully. Output is in ${FINAL_BAM}."
@@ -210,16 +214,13 @@ print_progress "Pipeline completed successfully. Output is in ${FINAL_BAM}."
 # Remove intermediate files if necessary
 if [ $KEEP_INTERMEDIATE -eq 0 ] && [ $POST_PROCESS -eq 1 ]; then
     print_progress "Removing intermediate files..."
-    rm -f $SORTED_BAM $MARKDUP_TXT $MARKDUP_BAM $MARKDUP_RG_BAM $RECAL
+    rm -f $RAW_BAM $MARKDUP_BAM "${MARKDUP_BAM}.bai" "${MARKDUP_BAM}.sbi" $RECAL 
 fi
 
-# View the final BAM file if the -v flag is set
-if [ $VIEW -eq 1 ]; then
-    print_progress "Producing .fai files required for IGV viewing..."
+# Index _raw.bam file (other .bam outputs indexed by post-processing tools)
+if [ $IGV -eq 1 ] && [ $POST_PROCESS -eq 0]; then
     samtools index $FINAL_BAM
-    if [ -f "${REF_FASTA}.fai" ]; then
-        samtools faidx $REF_FASTA
-    fi
 fi
+
 
 exit 1
