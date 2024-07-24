2906db24d53b112e59dfee9a96ab8cae3c36fa02
verammaz <110197814+verammaz@users.noreply.github.com>
2024-07-24 01:45:22 -0400

    moved from gatk4 to gatk3.6 to use indel realigner tools; updated some command line argument options

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-##############################################
-#specify file path for sites_of_variation.vcf 
-SITES_OF_VARIATION=
-##############################################
+######################### Change the following ! #############################
+# specify file path for reference fasta file
+REF_FASTA="/sc/arion/projects/FLAI/vera/References/ref_b37/human_g1k_v37_decoy.fasta"
+
+# specify file path for sites_of_variation.vcf 
+SITES_OF_VARIATION="/sc/arion/projects/FLAI/vera/References/dbsnp_138.b37.vcf"
+
+# specify file paths for reference files for indel realignment
+INDELS_1="/sc/arion/projects/FLAI/vera/References/1000G_phase1.indels.b37.vcf"
+INDELS_2="/sc/arion/projects/FLAI/vera/References/Mills_and_1000G_gold_standard.indels.b37.vcf"
+
+# specify path to exome target intervals file
+EXOME_INTERVALS="/sc/arion/projects/FLAI/vera/References/Broad.human.exome.b37.interval_list"
+
+# specify temporary directory, and make sure it has enough disk space 
+# recommendation: use /sc/arion/scratch 
+TEMP_DIR="/sc/arion/scratch/mazeev01/temp"
+
+##############################################################################
+
 
 # Function to print progress with timestamp
 print_progress() {
     echo "[`date +%Y-%m-%dT%H:%M:%S`] $1"
 }
-
-# Load required modules
-module --force purge
-print_progress "Loading required modules..."
-module load samtools >/dev/null 2>&1
-module load bwa >/dev/null 2>&1
-module load gatk >/dev/null 2>&1
-module load java/1.8.0_66 >/dev/null 2>&1
-module load picard/2.2.4 >/dev/null 2>&1
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -28,17 +35,18 @@ usage() {
 Usage: $0 [options] -f <reference.fa> -r <read1.fastq,read2.fastq> -o <output_prefix>
 
 Required Arguments:
-  -f <reference.fa>                Reference genome file in FASTA format.
   -r <read1.fastq,read2.fastq>     Comma-separated list of two FASTQ files.
   -o <output_prefix>               Prefix for output files.
+  -p <patient_id>
 
 Options:
   -h                               Display this message.
   -v                               Enable verbose mode.
   --index_ref                      Run bwa_index step.
   --keep_intermediate              Keep intermediate files.
-  --igv                            Index BAM files for IGV viewing.
   --post_process                   Carry out post processing of BAM file after alignment.
+  --threads                        Number of threads to use for bwa-mem step.
+  --step                           Specify starting step.
 
 
 EOF
@@ -46,14 +54,15 @@ EOF
 }
 
 # Variables
+PATIENT=
 READS=()
-REF_FASTA=
 OUTPUT_PREFIX=
-IGV=0
 VERBOSE=0
 INDEX=0
 POST_PROCESS=0
 KEEP_INTERMEDIATE=0
+THREADS=8
+STEP=0
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
@@ -61,19 +70,20 @@ while [[ "$#" -gt 0 ]]; do
         -h) usage ;;
         -v) VERBOSE=1 ;;
         --keep_intermediate) KEEP_INTERMEDIATE=1 ;;
-        --igv) IGV=1 ;;
         --post_process) POST_PROCESS=1 ;;
         --index_ref) INDEX=1 ;;
-        -f) REF_FASTA="$2"; shift ;;
+        --threads) THREADS="$2"; shift ;;
+        --step) STEP="$2"; shift ;;
         -r) READS="$2"; shift ;;
         -o) OUTPUT_PREFIX="$2"; shift ;;
+        -p) PATIENT="$2"; shift ;;
         *) echo "Error: Unkown argument/option: $1" ; usage ;;
     esac
     shift
 done
 
 # Check that mandatory arguments are provided
-if [ -z "$REF_FASTA" ] || [ -z "$READS" ] || [ -z "$OUTPUT_PREFIX" ]; then
+if [ "$STEP" -eq 0 ] && [ -z "$READS" ] || [ -z "$OUTPUT_PREFIX" ] || [ -z "$PATIENT" ]; then
     echo "Error: Not all required arguemnts provided."
     usage
 fi
@@ -82,7 +92,7 @@ fi
 IFS=',' read -r -a READ_ARRAY <<< "$READS"
 
 # Ensure exactly two read files are provided
-if [ ${#READ_ARRAY[@]} -ne 2 ]; then
+if [ "$STEP" -eq 0 ] && [ ${#READ_ARRAY[@]} -ne 2 ]; then
     echo "Error: Exactly two read files must be specified, separated by a comma."
     usage
 fi
@@ -91,17 +101,19 @@ READS_1=${READ_ARRAY[0]}
 READS_2=${READ_ARRAY[1]}
 
 # Check fastq read files
-if [ ! -f "$READS_1" ] || [ ! -f "$READS_2" ]; then
+if [ "$STEP" -eq 0 ] && [ ! -f "$READS_1" ] || [ ! -f "$READS_2" ]; then
     echo "Error: One or both fastq read files do not exist or are not readable."
     exit 1
 fi
 
 # Check reference genome file
 if [ ! -f "${REF_FASTA}" ]; then
-    echo "Reference file not found!"
+    echo "Reference file ${REF_FASTA} not found!"
     exit 1
 fi
 
+basename=$(basename "$OUTPUT_PREFIX")
+TEMP_DIR="$TEMP_DIR/$basename"
 
 # If verbose mode is enabled, print the parameters
 if [ $VERBOSE -eq 1 ]; then
@@ -110,7 +122,6 @@ if [ $VERBOSE -eq 1 ]; then
     INDEX_STR=$( [ $INDEX -eq 1 ] && echo "true" || echo "false" )
     KEEP_INTERMEDIATE_STR=$( [ $KEEP_INTERMEDIATE -eq 1 ] && echo "true" || echo "false" )
     POST_PROCESS_STR=$( [ $POST_PROCESS -eq 1 ] && echo "true" || echo "false" )
-    IGV_STR=$( [ $IGV -eq 1 ] && echo "true" || echo "false" )
 
     echo "---------------------------------------------"
     echo "Reference: $REF_FASTA"
@@ -120,7 +131,7 @@ if [ $VERBOSE -eq 1 ]; then
     echo "Run Indexing: $INDEX_STR"
     echo "Post Processing: $POST_PROCESS_STR"
     echo "Keep Intermediate Files: $KEEP_INTERMEDIATE_STR"
-    echo "Produce IGV Files: $IGV_STR"
+    echo "Temporary Directory: $TEMP_DIR"
     echo "---------------------------------------------"
 fi
 
@@ -128,22 +139,33 @@ fi
 #############################################################################################
 #Run fastq-->bam pipeline
 
+# Load required modules
+module purge
+print_progress "Loading required modules..."
+module load samtools
+module load bwa 
+module load gatk/3.6-0
+module load java
+module load picard/2.2.4
+
 # File Names
 RAW_BAM="${OUTPUT_PREFIX}_raw.bam"
-MARKDUP_BAM="${OUTPUT_PREFIX}_markdup.bam"
-MARKDUP_TXT="${OUTPUT_PREFIX}_markdup_metrics.txt"
+MARKDUP_BAM="${OUTPUT_PREFIX}_md.bam"
+MARKDUP_TXT="${OUTPUT_PREFIX}_md_metrics.txt"
+REALIGNED_BAM="${OUTPUT_PREFIX}_realign.bam"
 RECAL="${OUTPUT_PREFIX}_recal_data.table"
-FINAL_BAM="${OUTPUT_PREFIX}_final.bam"
+FINAL_BAM="${OUTPUT_PREFIX}.bam"
+
+# Make temporary directory if doesn't exist
+[ -d "$TEMP_DIR" ] || mkdir -p "$TEMP_DIR"
 
 echo "---------------------------------------------"
 print_progress "Starting the pipeline..."
 echo "---------------------------------------------"
 
-set -o pipefail 
 
-if [ $INDEX -eq 1 ]; then
+if [ $STEP -eq 0 ] && [ $INDEX -eq 1 ]; then
 # As indexing reference takes time and is only required once per reference, double check that indexing step is required:)
-    # Checking for index file in same directory as provided reference fasta file
     if [ ! -f "$REF_FASTA.amb" ] || \
            [ ! -f "$REF_FASTA.ann" ] || \
            [ ! -f "$REF_FASTA.bwt" ] || \
@@ -151,15 +173,33 @@ if [ $INDEX -eq 1 ]; then
            [ ! -f "$REF_FASTA.sa" ]; then
         print_progress "Indexing the reference..."
         bwa index -a bwtsw $REF_FASTA
+    else
+        echo "Skipping indexing step. Ran with --index_ref flag, but index files found!"
     fi
 fi
+
+# Check that required files for post processing are accesable
+if [ $POST_PROCESS -eq 1 ]; then
+    if [ ! -f "$SITES_OF_VARIATION" ]; then
+        echo "Error: Sites of variation ${SITES_OF_VARIATION} file not found. Required for post-processing base recalibration step."
+        exit 1
+    fi
+    if [ ! -f "$INDELS_1" ] || [ ! -f "$INDELS_2" ]; then
+        echo "Error: Indel reference file(s) not found. Required for post-processing indel realignment step."
+        exit 1
+    fi
+fi
+
+
+# TODO: add more error checking before pipeline start (maybe make a separate script for that)
+
 
 # Function to get verbosity flag for a given tool
 get_verbosity_flag() {
     local tool_name=$1
     if [ "$VERBOSE" -eq 0 ]; then
         case $tool_name in
-            bwa) echo "-v 1" ;;
+            bwa) echo "-v 0" ;;
             markdup) echo "VERBOSITY=ERROR" ;;
             baserecal) echo "--verbosity ERROR" ;;
             *) echo "" ;;
@@ -182,80 +222,128 @@ construct_read_group_id() {
 }
 
 
+################################ Run the pipeline ! #####################################
+#########################################################################################
+
 # Step 1: BWA MEM Alignment and Coordinate Sorting
 
-# Construct the read group ID
-id=$(construct_read_group_id "$READS_1")
-# Construct read group sample name 
-sample=$(basename "$READS_1" | cut -d'_' -f1)
+# Double check that index files are present
+if [ $STEP -eq 0 ] && [ ! -f "$REF_FASTA.amb" ] || [ ! -f "$REF_FASTA.ann" ] || [ ! -f "$REF_FASTA.bwt" ] || [ ! -f "$REF_FASTA.pac" ] || [ ! -f "$REF_FASTA.sa" ]; then
+    echo "Error: bwa index files not found. Run with the --index_ref flag."
+    exit 1
+fi
 
-print_progress "Aligning (bwa-mem) and sorting (samtools sort)"
-bwa mem -M -t 8 $REF_FASTA $READS_1 $READS_2 \
-        -R "@RG\tID:${id}\tSM:${sample}\tPL:ILLUMINA" $(get_verbosity_flag bwa) | samtools sort -@8 - -o $RAW_BAM
-wait   
+if [ $STEP -eq 0 ]; then
+    # Construct the read group ID
+    id=$(construct_read_group_id "$READS_1")
+    # Construct read group sample name 
+    sample=$(basename "$READS_1" | cut -d'_' -f1)
 
-if [ $POST_PROCESS -eq 1 ]; then
+    print_progress "Aligning (bwa-mem) and sorting (samtools sort)"
+    bwa mem -M -t $THREADS $REF_FASTA $READS_1 $READS_2 \
+            -R "@RG\tID:${id}\tSM:${PATIENT}_${sample}\tPL:ILLUMINA" $(get_verbosity_flag bwa) | samtools sort -T $TEMP_DIR "-@${THREADS}" - -o $RAW_BAM
+    wait
+    STEP=1
+fi
 
-    # Step 2: Picard MarkDuplicates
-    print_progress "Marking duplicates (Picard MarkDuplicates)..."
-    java -jar $PICARD MarkDuplicates \
-        I=$RAW_BAM \
-        O=$MARKDUP_BAM \
-        M=$MARKDUP_TXT \
-        CREATE_INDEX=true \
-        $(get_verbosity_flag markdup)
-    wait  
-
-    if [ -f "$SITES_OF_VARIATION" ]; then
-
-        if [ ! -f "${REF_FASTA%.*}.dict" ]; then
-            gatk CreateSequenceDictionary -R $REF_FASTA
-        fi
-        if [ ! -f "${SITES_OF_VARIATION}.idx" ]; then
-            gatk IndexFeatureFile -I $SITES_OF_VARIATION
-        fi
-
-        #Step 3: GATK BaseRecalibrator
-        print_progress "Recalibrating bases (GATK BaseRecalibrator)..."
-        gatk BaseRecalibrator \
-            -I $MARKDUP_BAM \
-            -R $REF_FASTA \
-            --known-sites $SITES_OF_VARIATION \
-            -O $RECAL \
-            $(get_verbosity_flag baserecal)
-        wait  
-
-        # Step 4: GATK ApplyBQSR
-        print_progress "Applying base recalibaration (GATK ApplyBQSR)..."
-        gatk ApplyBQSR \
-            -I $MARKDUP_BAM \
-            --bqsr-recal-file $RECAL \
-            -O $FINAL_BAM \
-            $(get_verbosity_flag baserecal)
-    
-    else
-        echo "WARNING: Sites of variation file $SITES_OF_VARIATION not found."
-        echo "WARNING: Cannot run GATK BaseRecalibrator."
-        print_progress "Terminating post processing after GATK MarkDuplicatesSpark..."
-        mv $MARKDUP_BAM $FINAL_BAM
+if [ $STEP -eq 1 ]; then
+    if [ ! -f $RAW_BAM ]; then
+        echo: "Error: MarkDuplicates starting step requires raw bam file. ${RAW_BAM} not found."
+        exit 1
     fi
+    # Step 2: Picard MarkDuplicates
+        print_progress "Marking duplicates (Picard MarkDuplicates)..."
+        java -jar $PICARD MarkDuplicates \
+            I=$RAW_BAM \
+            O=$MARKDUP_BAM \
+            M=$MARKDUP_TXT \
+            CREATE_INDEX=true \
+            $(get_verbosity_flag markdup)
+        wait
+        STEP=2
+fi
+
+if [ $POST_PROCESS -eq 1 ] || [ $STEP -eq 2 ]; then
+
+    if [ $STEP -eq 2 ] && [ ! -f $MARKDUP_BAM ]; then
+        echo "Error: Indel realignment step requires marked dup bam file. ${MARKDUP_BAM} not found."
+        exit 1
+    fi
+
+    if [ $STEP -eq 2 ]; then
+        #Step 3a: GATK RealignerTargetCreator
+        print_progress "Creating realigned targets (GATK RealignerTargetCreator)..."
+        java -jar $GATK_JAR \
+            -T RealignerTargetCreator \
+            -R $REF_FASTA \
+            -known $INDELS_1 \
+            -known $INDELS_2 \
+            -I $MARKDUP_BAM \
+            -o "${OUTPUT_PREFIX}.intervals"
+        
+        wait 
+
+        #Step 3b: GATK IndelRealigner
+        java -jar $GATK_JAR \
+            -T IndelRealigner \
+            -R $REF_FASTA \
+            -I $MARKDUP_BAM \
+            -known $INDELS_1 \
+            -known $INDELS_2 \
+            -targetIntervals "${OUTPUT_PREFIX}.intervals" \
+            -o $REALIGNED_BAM
+
+        STEP=3
+    fi
+
+
+    if [ $STEP -eq 3 ] && [ ! -f $REALIGNED_BAM ]; then
+        echo "Error: Base recalibration step requires realigned bam file. ${REALIGNED_BAM} not found."
+        exit 1
+    fi
+
+    if [ $STEP -eq 3 ]; then
+        #Step 4a: GATK BaseRecalibrator
+        print_progress "Recalibrating bases (GATK BaseRecalibrator)..."
+        java -jar $GATK_JAR \
+             -T BaseRecalibrator \
+             -L $EXOME_INTERVALS \
+             -ip 100 \
+             -I $REALIGNED_BAM \
+             -R $REF_FASTA \
+             -knownSites $SITES_OF_VARIATION \
+             -o $RECAL
+         wait  
+
+        # Step 4b: GATK PrintReads
+        print_progress "Applying base recalibaration (GATK PrintReads)..."
+        java -jar $GATK_JAR \
+             -T PrintReads \
+             -I $REALIGNED_BAM \
+             -L $EXOME_INTERVALS \
+             -ip 100 \
+             -BQSR $RECAL \
+             -R $REF_FASTA \
+             -o $FINAL_BAM
+    fi
+        
 else
-    mv $RAW_BAM $FINAL_BAM
+    FINAL_BAM=$MARKDUP_BAM
 fi
 
 print_progress "Pipeline completed successfully. Output is in ${FINAL_BAM}."
 
 
+# Delete temporary directory
+rm -r $TEMP_DIR
+
 # Remove intermediate files if necessary
-if [ $KEEP_INTERMEDIATE -eq 0 ] && [ $POST_PROCESS -eq 1 ]; then
+if [ $KEEP_INTERMEDIATE -eq 0 ]; then
     print_progress "Removing intermediate files..."
-    rm -f $RAW_BAM $MARKDUP_BAM "${MARKDUP_BAM%.*}.bai" $MARKDUP_TXT $RECAL 
+    rm -f $RAW_BAM
+    if [ $POST_PROCESS -eq 1 ]; then
+        rm -f $MARKDUP_BAM "${MARKDUP_BAM}.bai" "${MARKDUP_BAM}.sbi" "${MARKDUP_BAM%.*}.bai" "${OUTPUT_PREFIX}.intervals" "${REALIGNED_BAM}" "${RECAL}"
+    fi
 fi
 
-# Index _raw.bam file (other .bam outputs indexed by post-processing tools)
-if [ $IGV -eq 1 ] && [ $POST_PROCESS -eq 0]; then
-    samtools index $FINAL_BAM
-fi
-
-
-exit 1
+exit 0
