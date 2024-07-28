3d2eda78629edb03f192b23c52e925a82e1e3f4f
verammaz <110197814+verammaz@users.noreply.github.com>
2024-07-28 01:49:28 -0400

    removing hooks

--- hooks/post_merge.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p .git/hooks
-cat << 'EOF' > .git/hooks/post-merge
-#!/bin/bash
-if [ ! -f config.sh ]; then
-    echo "It looks like config.sh is missing. Please copy config.template.sh to config.sh and customize it."
-fi
-EOF
-chmod +x .git/hooks/post-merge
--- setup-hooks.sh
+++
@@ -1,6 +0,0 @@
-cat << 'EOF' > setup-hooks.sh
-#!/bin/bash
-cp hooks/post-merge .git/hooks/post-merge
-chmod +x .git/hooks/post-merge
-EOF
-chmod +x setup-hooks.sh
