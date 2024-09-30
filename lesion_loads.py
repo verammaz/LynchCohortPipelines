733373f9c3a6832a2ed722ebd65a79ad564f5443
verammaz <110197814+verammaz@users.noreply.github.com>
2024-09-30 15:11:32 -0400

    working on annotation checking

--- lesion_loads.py
+++ lesion_loads.py
@@ -46,7 +46,7 @@ def get_neoantigens(patient, hdir, kd=500):
         f = open(file, 'r')
         f.readline()
         for line in f.readlines():
-            variant = line.split('\t')[1]
+            variant = line.split('\t')[1] if 'other' not in file else ('\t').join(line.split('\t')[1][:-1])
             neo = line.split('\t')[3]
             score = line.split('\t')[6]
             if float(score) >= float(kd): # TODO: check this with Matt
