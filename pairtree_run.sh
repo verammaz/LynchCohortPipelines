1cae411d9cab043da5ead384b86610e1d32d2614
verammaz <110197814+verammaz@users.noreply.github.com>
2025-03-02 20:21:06 -0500

    fixing issue with pairtree running

--- pairtree_run.sh
+++ pairtree_run.sh
@@ -3,7 +3,7 @@
 scripts_path=$1
 
 module purge  
-module load python 
+module load python/3.10 
 
 python -c "import scipy; import numba; print(f'SciPy: {scipy.__version__}, Numba: {numba.__version__}')"
 
