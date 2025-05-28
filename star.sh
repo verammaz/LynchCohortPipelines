78d57ca830df4a0c825ce6783b662cfce039d325
verammaz <110197814+verammaz@users.noreply.github.com>
2025-05-28 13:07:38 -0700

    testing STAR

--- star.sh
+++ star.sh
@@ -21,6 +21,7 @@ create_directory "$GENOME_DIR"
 STAR --runMode genomeGenerate \
              --runThreadN 8 \
              --genomeDir ${GENOME_DIR} \
+             --outFileNamePrefix "${GENOME_DIR}/star_" \
              --genomeFastaFiles ${REF_FASTA} \
              --sjdbGTFfile ${GTF_FILE} \
              --sjdbOverhang 100  # Adjust depending on read length - 1
